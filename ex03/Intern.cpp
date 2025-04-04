db76d4da5ab4c4f844156cacb8eb5dc93405056b
Jihyun Jeong <jihyjeon@cx1r4s8.42seoul.kr>
2025-04-04 17:09:19 +0900

    intern copy constructor mod

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -16,7 +16,7 @@ Intern::~Intern() {
 Intern& Intern::operator=(const Intern& other) {
 	std::cout << std::endl << GREY << "Intern assignment operator called\n" << RESET;
 	if (this != &other)
-		;
+		return *this;
 	return *this;
 }
 
